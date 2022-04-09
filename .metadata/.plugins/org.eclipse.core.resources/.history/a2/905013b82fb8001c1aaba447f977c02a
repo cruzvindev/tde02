0d6dfe90e98d5c8d8f265b3b70e741f31fc38067
Vinicius Cruz <88784671+cruzvindev@users.noreply.github.com>
2022-04-09 15:15:04 -0300

    TDE 02

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a2/905013b82fb8001c1aaba447f977c02a
@@ -0,0 +1,35 @@
+package Matematica;
+
+public class Retangulo {
+	private static double ladoH;
+	private static double ladoV;
+	
+	public static double getLadoH() {
+		return ladoH;
+	}
+
+	public static void setLadoH(double ladoH) {
+		Retangulo.ladoH = ladoH;
+	}
+
+	public static double getLadoV() {
+		return ladoV;
+	}
+
+	public static void setLadoV(double ladoV) {
+		Retangulo.ladoV = ladoV;
+	}
+
+	public static void definirLados(double ladoH, double ladoV) {
+		setLadoV(ladoV);
+		setLadoH(ladoH);
+	}
+	
+	public static double area() {
+		return getLadoV() * getLadoH();
+	}
+	
+	public static double perimetro() {
+		return (getLadoV()*2) + (getLadoH()*2);
+	}
+}
---
+++ test/src/Especialista/EngenheiroCivil.java
@@ -0,0 +1,52 @@
+package Especialista;
+
+
+
+import Matematica.Retangulo;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class EngenheiroCivil {
+	
+	private static String nome;
+	private static int idade;
+
+	public void setIdade(int idade) {
+		EngenheiroCivil.idade = idade;
+	}
+
+	public void setNome(String nome) {
+		EngenheiroCivil.nome = nome;
+	}
+	
+	public void exibirDadosPessoais() {
+		System.out.println("DADOS PESSOAIS");
+		System.out.println(nome);
+		System.out.println(idade + " anos");
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		EngenheiroCivil engenheiro = new EngenheiroCivil();
+		
+		System.out.print("Nome do engenheiro: ");
+		String nome = scan.nextLine();
+		engenheiro.setNome(nome);
+		
+		System.out.print("Idade do engenheiro: ");
+		int idade = scan.nextInt();
+		engenheiro.setIdade(idade);
+		
+		engenheiro.exibirDadosPessoais();
+		System.out.println("DADOS DO RETÂNGULO");
+		Retangulo.definirLados(4.7, 8.2);
+		System.out.printf("Area: %.2f\n", Retangulo.area());
+		System.out.printf("Perimetro: %.2f\n", Retangulo.perimetro());
+		
+		scan.close();
+	}
+
+}
